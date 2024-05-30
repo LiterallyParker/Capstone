84400810b801d9fb09f2cbc2b877ddb5f4c23f8e
LiterallyParker <parkernash2001@gmail.com>
2024-05-29 23:43:39 -0500

    authentication 1.1

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -1,14 +1,15 @@
 const jwt = require("jsonwebtoken");
-const secretKey = "paiohuqmetbnmueaxcpioveqdcvnb";
+const key = "paiohuqmetxcpioveqdcvnb";
 
 async function createToken(userObject) {
 
   try {
-    const token = await jwt.sign({ userId: userObject.id }, secretKey);
+    const token = await jwt.sign({ userId: userObject.id }, key);
     return token;
   } catch (error) {
     console.error(error);
   };
+
 };
 
 async function checkForCorrectUser(req, res, next) {
@@ -19,7 +20,7 @@ async function checkForCorrectUser(req, res, next) {
   };
 
   try {
-    const payload = jwt.verify(token, secretKey);
+    const payload = jwt.verify(token, key);
 
     if (payload.userId !== +req.params.userId) {
       return next(new Error('Unauthorized'));
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const client = require('./db/index')
+client.connect()
 
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+const apiRoutes = require("./api");
+app.use('/api', apiRoutes);
+
+app.get('*', (req, res) => {
+  res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
+});
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
