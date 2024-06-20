429a00929efdce2fd3ae46c4df4ee946ad24f50a
LiterallyParker <parkernash2001@gmail.com>
2024-06-20 15:28:34 -0500

    deployment

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,15 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-const client = new Client(process.env.DATABASE_URL || "");
+const production = process.env.NODE_ENV === "production"
+const client = new Client(
+  {
+    host: production ? process.env.DATABASE_HOSTNAME : process.env.DEV_HOSTNAME,
+    port: process.env.DATABASE_PORT,
+    database: production ? process.env.DATABASE_HOSTNAME : process.env.DEV_DATABASE,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD
+  }
+);
 
 module.exports = client;
--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,12 @@ server.use('/api', apiRoutes);
 server.get('*', (req, res, next) => {
   res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
 });
+
 const PORT = process.env.PORT || 3000;
 server.use((error, req, res, next) => {
   console.error('SERVER ERROR: ', error);
   if(res.statusCode < 400) res.status(500);
   res.send({error: error.error, name: error.name, message: error.message, table: error.table});
 });
+
 server.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
