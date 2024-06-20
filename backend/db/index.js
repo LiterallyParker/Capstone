3679d6684d688ef0450720c5ba3e5fbd39505937
LiterallyParker <parkernash2001@gmail.com>
2024-06-20 16:28:33 -0500

    deployment

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,17 +2,21 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 // const production = process.env.NODE_ENVIRONMENT === "production";
-const production = true
-const client = new Client(
-  {
-    host: production ? process.env.DATABASE_HOSTNAME : process.env.DEV_HOSTNAME,
-    port: process.env.DATABASE_PORT,
-    database: production ? process.env.DATABASE_HOSTNAME : process.env.DEV_DATABASE,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    ssl: { rejectUnauthorized: false }
-  }
-);
+// const production = true
+// const client = new Client(
+  // {
+  //   // host: production ? process.env.DATABASE_HOSTNAME : process.env.DEV_HOSTNAME,
+  //   port: process.env.DATABASE_PORT,
+  //   database: production ? process.env.DATABASE_HOSTNAME : process.env.DEV_DATABASE,
+  //   user: process.env.DATABASE_USERNAME,
+  //   password: process.env.DATABASE_PASSWORD,
+  //   ssl: { rejectUnauthorized: false }
+  // }
+// );
+
+const connectionString = process.env.DATABASE_URL;
+
+const client = new Client({connectionString, ssl:{rejectUnauthorized:false}})
 
 client.on("connect", () => {console.log("connected.")})
 client.on("error", (error) => {
